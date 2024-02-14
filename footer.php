70d0c1264d0f5eff7ee63beadc9c0e72f39d5eb4
Sana842 <khairunshaikh16847@gmail.com>
2024-02-14 15:47:25 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Footer Design</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+ <!-- Customized Bootstrap Stylesheet -->
+ <link href="footer.css" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col">
+                    <h4>company Name</h4>
+                    <ul>
+                        <li><a href="#">about us</a></li>
+                        <li><a href="#">our services</a></li>
+                        <li><a href="#">privacy policy</a></li>
+                        <li><a href="#">affiliate program</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>Quick Links</h4>
+                    <ul>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">Terms & Condition</a></li>
+                        <li><a href="#">FAQs & Help</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>Contact</h4>
+                    <ul id="tt1">
+                        <li>
+                            <a href="#"> <i class="fa-solid fa-location-dot"></i>   123 Street, New York, USA</a>
+                        </li>
+                        <li><a href="#"> <i class="fa-solid fa-phone"></i>    + 0123456790</a></li>
+                        <li><a href="#"> <i class="fa-solid fa-envelope"></i>    interior@gmail.com</a></li>
+
+                        <div class="social-links">
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </ul>
+
+
+                </div>
+                <div class="footer-col">
+                    <h4>Gallery</h4>
+
+                    <div class="col-4">
+                        <div class="row22">
+                            <div class="column22">
+                                <img src="./img/foot1" alt="Snow" style="width:100%">
+                            </div>
+                            <div class="column22">
+                                <img src="./img/foot2" alt="Forest" style="width:100%">
+                            </div>
+                            <div class="column22">
+                                <img src="./img/foot3" alt="Mountains" style="width:100%">
+                            </div>
+                        </div>
+
+
+                        <div class="column22">
+                            <img src="./img/foot4" alt="Snow" style="width:100%">
+                        </div>
+                        <div class="column22">
+                            <img src="./img/foot5" alt="Forest" style="width:100%">
+                        </div>
+                        <div class="column22">
+                            <img src="./img/foot6" alt="Mountains" style="width:100%">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </footer>
+
+</body>
+
+</html>
